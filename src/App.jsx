3b8a11083a8eb99fbad0c3f16eb759661d725fa6
Salih AKSU <salihaksu66@gmail.com>
2024-02-12 16:50:44 +0300

    Yazı Değişimi Tamamlandı

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Archi's Academy GitHub Kursuna Hoşgeldiniz!
         </p>
       </div>
       <p className="read-the-docs">
